82224ca76c095ae1941759d0d6413a7cefefa614
Eric Ray <erichamion@gmail.com>
2022-04-19 19:11:29 -0500

    first commit

---
+++ ClassesHomework/Zoo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesHomework
+{
+    // One of the tests will tell you to uncomment the Zoo class.
+    // Aside from uncommenting when you get to that test,
+    // don't change any code in this file.
+
+
+    //class Zoo
+    //{
+
+    //    private Dictionary<string, Animal> _myAnimals = new Dictionary<string, Animal>();
+    //    public string Name { get; } 
+    //    public Zoo(string zooName)
+    //    {
+    //        Name = zooName;
+    //    }
+
+    //    public void AddAnimal(string name, Animal anim)
+    //    {
+    //        _myAnimals.Add(name, anim);
+    //    }
+
+    //    public void SpeakNOW()
+    //    {
+    //        foreach(KeyValuePair<string, Animal> kvp in _myAnimals)
+    //        {
+    //            Console.WriteLine($"{kvp.Key} says {kvp.Value.Speak()}");
+    //        }
+    //    }
+
+    //    public double GetAverageAge()
+    //    {
+    //        if (_myAnimals.Count == 0)
+    //        {
+    //            return 0;
+    //        }
+
+    //        int sum = 0;
+    //        foreach(KeyValuePair<string, Animal> kvp in _myAnimals)
+    //        {
+    //            sum += kvp.Value.Age;
+    //        }
+    //        return (double)sum / _myAnimals.Count;
+    //    }
+    //}
+}
