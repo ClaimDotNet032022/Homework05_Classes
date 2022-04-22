9e04445a42853179661be8e81ac3b84d2cb2fee5
bettag1579 <bettag1579@yahoo.com>
2022-04-21 19:06:46 -0500

    commitClasswesHWdraft

---
+++ ClassesHomework/Animal1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassesHomework
+{
+    internal class Animal
+    {
+        public string Species { get; internal set; }
+
+        public static implicit operator Animal(bool v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static implicit operator Animal(Dog v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static implicit operator Animal(Cat v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string Speak()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ClassesHomework/Dog.cs
+++ ClassesHomework/Dog.cs
@@ -6,9 +6,49 @@ using System.Threading.Tasks;
 
 namespace ClassesHomework
 {
-    // Write your dog class here.
-    //class Dog
-    //{
-        
-    //}
+    internal class Dog
+    {
+        public Dog()
+        {
+        }
+
+        public Dog(string v)
+        {
+            V = v;
+        }
+
+        public Dog(string v, int v1) : this(v)
+        {
+        }
+
+        public string Breed { get; internal set; }
+        public string V { get; }
+        public int Age { get; internal set; }
+        public bool IsSitting { get; internal set; }
+
+        internal string Speak()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string Speak(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Sit()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Stand()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void CelebrateBirthday()
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
